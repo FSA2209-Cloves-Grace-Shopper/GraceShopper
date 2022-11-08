cc07a0d82d134adc0a215641b1d4ac7d2461d411
Peter Rodocker <peterrodocker@gmail.com>
2022-11-08 09:02:41 -0800

    Added Product Model

--- server/db/index.js
+++ server/db/index.js
@@ -3,6 +3,7 @@
 const db = require('./db')
 
 const User = require('./models/User')
+const Product = require('./models/Product')
 
 //associations could go here!
 
@@ -10,5 +11,6 @@ module.exports = {
   db,
   models: {
     User,
+    Product
   },
 }
