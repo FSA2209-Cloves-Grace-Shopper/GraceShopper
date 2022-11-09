7120d50b3622828d7d4318c1c90e26f48e4897aa
sirwin6 <97652800+sirwin6@users.noreply.github.com>
2022-11-09 09:51:59 -0800

    Revert "User routes"

--- server/api/users.js
+++ server/api/users.js
@@ -1,66 +1,17 @@
-const router = require("express").Router();
-const {
-  models: { User },
-} = require("../db");
-module.exports = router;
+const router = require('express').Router()
+const { models: { User }} = require('../db')
+module.exports = router
 
-// Get all users /api/users
-router.get("/", async (req, res, next) => {
+router.get('/', async (req, res, next) => {
   try {
     const users = await User.findAll({
-      attributes: ["id", "email"],
-    });
-    res.json(users);
+      // explicitly select only the id and username fields - even though
+      // users' passwords are encrypted, it won't help if we just
+      // send everything to anyone who asks!
+      attributes: ['id', 'username']
+    })
+    res.json(users)
   } catch (err) {
-    next(err);
+    next(err)
   }
-});
-// Get single user /api/users/:userId
-router.get("/:userId", async (req, res, next) => {
-  try {
-    const user = await User.findByPk(req.params.userId, {
-      attributes: ["id", "email"],
-    });
-    res.json(user);
-  } catch (err) {
-    next(err);
-  }
-});
-
-// Update user /api/users/:userId
-router.put("/:userId", async (req, res, next) => {
-  try {
-    const updateUser = await User.update(req.body, {
-      where: {
-        id: req.params.userId,
-      },
-      returning: true,
-    });
-    res.send(updateUser);
-  } catch (err) {
-    next(err);
-  }
-});
-// Add new user /api/users
-router.post("/", async (req, res, next) => {
-  try {
-    const newUser = await User.create(req.body);
-    res.json(newUser);
-  } catch (err) {
-    next(err);
-  }
-});
-
-// Remove user /api/users/:userId
-router.delete("/:userId", async (req, res, next) => {
-  try {
-    await User.destroy({
-      where: {
-        id: req.params.userId,
-      },
-    });
-    res.sendStatus(204);
-  } catch (err) {
-    next(err);
-  }
-});
+})
