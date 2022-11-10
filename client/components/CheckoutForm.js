ade4f436da2704244764924530cd7174de66e7f5
Jerry Wu <jerrywu.hr@gmail.com>
2022-11-09 16:29:44 -0800

    react hook form conversion

--- client/components/CheckoutForm.js
+++ client/components/CheckoutForm.js
@@ -1,38 +1,35 @@
 import React, { useState } from "react";
+import { useForm } from "react-hook-form";
 
-const CheckoutForm = () => {
-  const [state, setState] = useState({
-    firstName: "",
-    lastName: "",
-    address: "",
-    email: "",
+const CheckoutForm = ({ user = {} }) => {
+  const { register, handleSubmit } = useForm({
+    defaultValues: user,
   });
-
-  const handleSubmit = (event) => {
-    event.preventDefault();
+  const handleSave = (formVal) => {
+    console.log(formVal);
   };
-
-  const handleChange = (event) => {
-    const name = event.target.name;
-    const value = event.target.value;
-    setState({ ...state, [name]: value });
-  };
-
   return (
-    <form onSubmit={handleSubmit}>
+    <form onSubmit={handleSubmit(handleSave)}>
       <h1>Check Out Form</h1>
-
-      <label>First Name</label>
-      <input type="text" name="firstName" onChange={handleChange} />
-
-      <label>Last Name</label>
-      <input type="text" name="lastName" onChange={handleChange} />
-      <label>Address</label>
-      <input type="text" name="address" onChange={handleChange} />
-      <label>email</label>
-      <input type="text" name="email" onChange={handleChange} />
-
-      <input type="submit" />
+      <div>
+        <label>First Name</label>
+        <input type="text" {...register("firstName")} />
+      </div>
+      <div>
+        <label>Last Name</label>
+        <input type="text" {...register("lastName")} />
+      </div>
+      <div>
+        <label>Address</label>
+        <input type="text" {...register("address")} />
+      </div>
+      <div>
+        <label>email</label>
+        <input type="text" {...register("email")} />
+      </div>
+      <div>
+        <button type="submit">Submit Form</button>
+      </div>
     </form>
   );
 };
