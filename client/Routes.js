7df5294e6d31ae1d04c6e2bfe686f9a52715ac37
sirwin6 <irwins06@gmail.com>
2022-11-10 08:48:35 -0800

    styles for singleProduct view

--- client/Routes.js
+++ client/Routes.js
@@ -4,6 +4,7 @@ import { withRouter, Route, Switch, Redirect } from 'react-router-dom';
 import { Login, Signup } from './components/AuthForm';
 import Home from './components/Home';
 import ViewSingleProduct from './components/ViewSingleProduct';
+import Cart from './components/Cart';
 import { me } from './store';
 
 /**
