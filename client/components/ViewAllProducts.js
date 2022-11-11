d7c5b261129acc44df3d041a488bd765b60d6f6a
sirwin6 <irwins06@gmail.com>
2022-11-11 11:54:40 -0800

    Some nav bar styling and links

--- client/components/ViewAllProducts.js
+++ client/components/ViewAllProducts.js
@@ -6,26 +6,27 @@ const ViewAllProducts = () => {
   const dispatch = useDispatch();
 
   useEffect(() => {
-    dispatch(fetchProducts())
+    dispatch(fetchProducts());
   }, []);
 
   const products = useSelector((state) => state.allProducts);
-  
+
   return (
     <div>
-      <h2 className="allProductsTitle">
-        {'PRODUCTS'}
-      </h2>
+      <h2 className="allProductsTitle">{'PRODUCTS'}</h2>
       // Clean this up
-      <ul>{products.map((product) => (
-        <li key={product.id}>
-          <p>Brand: {product.name}</p>
-          <p>Type: {product.type}</p>
-          <p>Style: {product.style}</p>
-          <p>Price: {product.price}</p>
-          <img src={product.imageUrl}/>
-        </li> 
-      ))}
+      <ul>
+        {products.map((product) => (
+          <li key={product.id}>
+            <p>Brand: {product.name}</p>
+            <p>Type: {product.type}</p>
+            <p>Style: {product.style}</p>
+            <p>Price: {product.price}</p>
+            <a href={`/products/${product.id}`}>
+              <img src={product.imageUrl} />
+            </a>
+          </li>
+        ))}
       </ul>
     </div>
   );
