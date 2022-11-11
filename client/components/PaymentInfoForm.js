4e8a853bf76ba29c244fb2f42af24c7ae3df6988
Jerry Wu <jerrywu.hr@gmail.com>
2022-11-11 15:19:57 -0800

    form validation implemented, added footer.

--- client/components/PaymentInfoForm.js
+++ client/components/PaymentInfoForm.js
@@ -1,23 +1,10 @@
 import React, { useState } from 'react';
 import { useForm } from 'react-hook-form';
-import { zodResolver } from '@hookform/resolvers/zod';
-import { string, z } from 'zod';
-
-const schema = z.object({
-  firstName: string().min(1, { message: 'First name is required' }),
-  lastName: string().min(1, { message: 'Last name is required' }),
-  email: string().email(),
-  address: string().min(1, { message: 'Address is required' }),
-});
 
 const CheckoutForm = ({ user = {} }) => {
-  const { register, handleSubmit, formState } = useForm({
+  const { register, handleSubmit } = useForm({
     defaultValues: user,
-    resolver: zodResolver(schema),
   });
-
-  const { errors } = formState;
-
   const handleSave = (formVal) => {
     console.log(formVal);
   };
@@ -27,22 +14,18 @@ const CheckoutForm = ({ user = {} }) => {
       <div>
         <label>First Name</label>
         <input type="text" {...register('firstName')} />
-        <div style={{ color: 'red' }}>{errors.firstName?.message}</div>
       </div>
       <div>
         <label>Last Name</label>
         <input type="text" {...register('lastName')} />
-        <div style={{ color: 'red' }}>{errors.lastName?.message}</div>
       </div>
       <div>
         <label>Address</label>
         <input type="text" {...register('address')} />
-        <div style={{ color: 'red' }}>{errors.address?.message}</div>
       </div>
       <div>
         <label>email</label>
         <input type="text" {...register('email')} />
-        <div style={{ color: 'red' }}>{errors.email?.message}</div>
       </div>
       <div>
         <button type="submit">Submit Form</button>
